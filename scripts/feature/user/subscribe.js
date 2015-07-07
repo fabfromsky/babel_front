e03215152897fc0ce04e37bd71c9812242b8699a
fdesert <fdesert@sii.fr>
2015-07-07 15:11:01 +0200

    inscription ok

---
+++ scripts/feature/user/subscribe.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+  $(".validate").on("click", function(){
+    var username = $(".login").val();
+    var password = $(".password").val();
+    var mail = $(".mail").val();
+    var firstname = $(".firstname").val();
+    var lastname = $(".lastname").val();
+
+    if(username == "") {
+      $(".login").addClass("required");
+    } else if(password == "") {
+      $(".password").addClass("required");
+    } else if(mail == "") {
+      $(".mail").addClass("required");      
+    } else if(firstname == "") {
+      $(".firstname").addClass("required");      
+    } else if(lastname == "") {
+      $(".lastname").addClass("required");      
+    } else {
+      var user = {
+        "username": username,
+        "firstName": firstname,
+        "lastName": lastname,
+        "pwd": password,
+        "mail": mail
+      }
+
+      var userStr = JSON.stringify(user);
+
+      persistUser(userStr);  
+    }
+    
+  })
+});
+
+function persistUser(user) {
+  debugger;
+  $.ajax({
+    type:"POST",
+    url: "http://localhost:8080/user/save",
+    contentType: "application/json",
+    dataType: "json",
+    data: user
+  }).then(redirect(user));
+}
+
+function redirect(user) {
+  var data = JSON.parse(user);
+  localStorage.setItem("username", data.username);
+  location.href = "index.html";
+}
